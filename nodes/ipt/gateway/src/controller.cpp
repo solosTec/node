0abca9e7e29826af75408baf889c4403cc7e9b21
Sylko Olzscher <sylko.olzscher@gmail.com>
2018-05-08 11:27:15 +0200

    write pid file

--- nodes/ipt/gateway/src/controller.cpp
+++ nodes/ipt/gateway/src/controller.cpp
@@ -25,7 +25,7 @@
 #include <cyng/scm/service.hpp>
 #endif
 #if BOOST_OS_LINUX
-#include <cyng/sys/process.h>
+#include "../../../write_pid.h"
 #endif
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/random_generator.hpp>
@@ -176,7 +176,7 @@ namespace node
 
 				, cyng::param_factory("hardware", cyng::tuple_factory(
 					cyng::param_factory("manufacturer", "solosTec"),	//	manufacturer (81 81 C7 82 03 FF)
-					cyng::param_factory("model", "Gateway"),	//	Typenschlüssel (81 81 C7 82 09 FF --> 81 81 C7 82 0A 01)
+					cyng::param_factory("model", "Gateway"),	//	TypenschlÃ¼ssel (81 81 C7 82 09 FF --> 81 81 C7 82 0A 01)
 					cyng::param_factory("serial", rgen()),	//	Seriennummer (81 81 C7 82 09 FF --> 81 81 C7 82 0A 02)
 					cyng::param_factory("class", "129-129:199.130.83*255")	//	device class (81 81 C7 82 02 FF) MUC-LAN/DSL
 					//cyng::param_factory("mac", "")	//	mac 
@@ -301,18 +301,7 @@ namespace node
 
 #if BOOST_OS_LINUX
         const boost::filesystem::path log_dir = cyng::value_cast<std::string>(dom.get("log-dir"), ".");
-        const auto pid = (log_dir / boost::uuids::to_string(tag)).replace_extension(".pid").string();
-        std::fstream fout(pid, std::ios::trunc | std::ios::out);
-        if (fout.is_open())
-        {
-            CYNG_LOG_INFO(logger, "write PID file: " << pid);
-            fout << cyng::sys::get_process_id();
-            fout.close();
-        }
-        else
-        {
-            CYNG_LOG_ERROR(logger, "could not write PID file: " << pid);
-        }
+        write_pid(log_dir, tag);
 #endif
 
         //
--- nodes/modem/src/controller.cpp
+++ nodes/modem/src/controller.cpp
@@ -18,7 +18,7 @@
 #include <cyng/dom/tree_walker.h>
 #include <cyng/async/task/task_builder.hpp>
 #if BOOST_OS_LINUX
-#include <cyng/sys/process.h>
+#include "../../write_pid.h"
 #endif
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/random_generator.hpp>
@@ -272,18 +272,7 @@ namespace node
 
 #if BOOST_OS_LINUX
         const boost::filesystem::path log_dir = cyng::value_cast<std::string>(dom.get("log-dir"), ".");
-        const auto pid = (log_dir / boost::uuids::to_string(tag)).replace_extension(".pid").string();
-        std::fstream fout(pid, std::ios::trunc | std::ios::out);
-        if (fout.is_open())
-        {
-            CYNG_LOG_INFO(logger, "write PID file: " << pid);
-            fout << cyng::sys::get_process_id();
-            fout.close();
-        }
-        else
-        {
-            CYNG_LOG_ERROR(logger, "could not write PID file: " << pid);
-        }
+        write_pid(log_dir, tag);
 #endif
 
 		//
