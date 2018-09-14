bae7b843b49a88c5b33482bfd332b881748968af
sylko.olzscher <CmdPirx@gmail.com>
2018-09-14 23:07:40 +0200

    m-bus units added

--- lib/mbus/protocol/lib.cmake
+++ lib/mbus/protocol/lib.cmake
@@ -3,21 +3,14 @@ set (mbus_protocol_lib)
 
 set (mbus_protocol_cpp
 
-#	lib/mbus/protocol/src/codes.cpp
-#	lib/mbus/protocol/src/header.cpp
-#	lib/mbus/protocol/src/response.cpp
 	lib/mbus/protocol/src/parser.cpp
-#	lib/mbus/protocol/src/serializer.cpp
+	lib/mbus/protocol/src/units.cpp
 )
 
 set (mbus_protocol_h
 	src/main/include/smf/mbus/defs.h
-#	src/main/include/smf/mbus/codes.h
-#	src/main/include/smf/mbus/header.h
-#	src/main/include/smf/mbus/response.hpp
-#	src/main/include/smf/mbus/scramble_key.h
 	src/main/include/smf/mbus/parser.h
-#	src/main/include/smf/mbus/serializer.h
+	src/main/include/smf/mbus/units.h
 )
 
 # define the main program
