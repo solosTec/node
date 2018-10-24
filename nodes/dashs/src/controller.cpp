1e4cb008d33a71f7a90ca7bd308cdcd55ec8eae3
Sylko Olzscher <sylko.olzscher@gmail.com>
2018-10-24 23:44:51 +0200

    dash/s use certificate files

--- nodes/dashs/src/controller.cpp
+++ nodes/dashs/src/controller.cpp
@@ -534,154 +534,82 @@ namespace node
 			boost::asio::ssl::context::single_dh_use);
 
 		try {
-			//ctx.use_certificate_chain_file(tls_certificate_chain);
-			//ctx.use_private_key_file(tls_private_key, boost::asio::ssl::context::pem);
-			//ctx.use_tmp_dh_file(tls_dh);
+			ctx.use_certificate_chain_file(tls_certificate_chain);
+			ctx.use_private_key_file(tls_private_key, boost::asio::ssl::context::pem);
+			ctx.use_tmp_dh_file(tls_dh);
 
-			//std::string const cert =
-			//	"-----BEGIN CERTIFICATE-----\n"
-			//	"MIIDijCCAnKgAwIBAgIJANvdlIO3NeJjMA0GCSqGSIb3DQEBCwUAMFoxCzAJBgNV\n"
-			//	"BAYTAkNIMQswCQYDVQQIDAJMVTEQMA4GA1UEBwwHTHVjZXJuZTERMA8GA1UECgwI\n"
-			//	"c29sb3NUZWMxGTAXBgNVBAMMEHd3dy5zb2xvc3RlYy5jb20wHhcNMTgxMDA5MTIz\n"
-			//	"NTUyWhcNNDYwMjI0MTIzNTUyWjBaMQswCQYDVQQGEwJDSDELMAkGA1UECAwCTFUx\n"
-			//	"EDAOBgNVBAcMB0x1Y2VybmUxETAPBgNVBAoMCHNvbG9zVGVjMRkwFwYDVQQDDBB3\n"
-			//	"d3cuc29sb3N0ZWMuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA\n"
-			//	"zYqQwxW1pk/L6SywQZ3ROZW855uc2sVYsa5zMeZWYGQ7Uj9+F8wRxRE90t68BAP0\n"
-			//	"Ox+Co2T8rN0SFArsEZszfqX85b3zwKKUCEBFu3Y1suLTGEgAa1KyW5j4OufSN5Rn\n"
-			//	"GAe0v2T/tBMmSA3cKfH+FS272fqYINy+e2Z6ut8PSxVswknppEpY7IYQAoVXQA8G\n"
-			//	"WedempWKb8N0rBv/ngSoWgM+ol+XKyedBUFrecsON6o4juDzT1LxsoHG/YdxhbbZ\n"
-			//	"9gx1dc01ouwAqYLKhbx2sqd5FNAVi7wGCZeX12zOtg08VrqK/MiRbOGI3xnp7YeT\n"
-			//	"kKwe///bVY/lCuLfmaTzawIDAQABo1MwUTAdBgNVHQ4EFgQU8QMsxw5pzpB4O3s6\n"
-			//	"PziQTYiIFW4wHwYDVR0jBBgwFoAU8QMsxw5pzpB4O3s6PziQTYiIFW4wDwYDVR0T\n"
-			//	"AQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAbqRJsKK5f6QKBo/iLVOeyVsg\n"
-			//	"0RTRHZJKyJQa5yBF2fF4VtGT1I0Qwrb8W8jA3BahmexVAQW5Wl+fmW/GTBllHREg\n"
-			//	"7c7NlfLRuZ7cFVXPe3DsONUQb+Qxl2d8Mrp06IPKh8SbRS0L/4LAxBygFi16+zTR\n"
-			//	"Z8o0a95fDXCAr4nPTguAkG4k0BweSxMSkZK5tfpC61+q3bHh0bz7dcbPOqdY3Kq+\n"
-			//	"jaZAXYB7gEUdXwFzL5TBtzHdo+jutvp79IlLqpIM674CkEzGFZh0FtKqvtMWH7d2\n"
-			//	"qfuzBAbYypxSrPdwjQ19teKZ51qsA7VwIHmeXcbuLfRJnySAyqlEYUeSJ5fxcw==\n"
-			//	"-----END CERTIFICATE-----\n";
 
-			//ctx.use_certificate_chain(
-			//	boost::asio::buffer(cert.data(), cert.size()));
-
-			//std::string const key =
-			//	"-----BEGIN PRIVATE KEY-----\n"
-			//	"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDNipDDFbWmT8vp\n"
-			//	"LLBBndE5lbznm5zaxVixrnMx5lZgZDtSP34XzBHFET3S3rwEA/Q7H4KjZPys3RIU\n"
-			//	"CuwRmzN+pfzlvfPAopQIQEW7djWy4tMYSABrUrJbmPg659I3lGcYB7S/ZP+0EyZI\n"
-			//	"Ddwp8f4VLbvZ+pgg3L57Znq63w9LFWzCSemkSljshhAChVdADwZZ516alYpvw3Ss\n"
-			//	"G/+eBKhaAz6iX5crJ50FQWt5yw43qjiO4PNPUvGygcb9h3GFttn2DHV1zTWi7ACp\n"
-			//	"gsqFvHayp3kU0BWLvAYJl5fXbM62DTxWuor8yJFs4YjfGenth5OQrB7//9tVj+UK\n"
-			//	"4t+ZpPNrAgMBAAECggEAKlO2q/X0imM0YrhJM7oENA+Z7zgkfQjW0wBRyJzefMsn\n"
-			//	"TDC3Dr9/iU2bpakonssRqHs8s9lAD3kXF/mH07haSmJ0SftsDudsBKb3tTsKo+QC\n"
-			//	"BKYPMAq/oA1IURkcOUXeVRyBJEjwP20SHRR9mkWzRTk9zg3E9j9/GnpoG8ODqrpI\n"
-			//	"K6tJQDlUj5vuqIPiweGrfVeCTN/OoApilyknTGPIBce00Ui8ay8z0ioj485fllpI\n"
-			//	"evM9G7CFliLZ5bDfwhDqrz/0tK+SVvTJesC+3ryqPg46FywDqqQ+P8AM8ilHdF93\n"
-			//	"oHZIatP4f9VVDrggRhI9zHzNUiXiDjhAaXw+Nlq3AQKBgQDxB/bCgFciu3iy5bb+\n"
-			//	"e1s6zsWPqrIrJ6yRhJ19qgydp4t4i7YLnAyPMSmHgm3jWe2YIpvpTbERcDSelDPs\n"
-			//	"dzGcsRN2Te2KuI2vy+urLJadUfTL7GXUY5lY4EvBi7ptIo7m038nrE5UCufwnFh4\n"
-			//	"dkK7kd2E39Z7kpHu4rrXrUor+wKBgQDaTl2uld1E5LaYqlbbxRLW8YFqsyYTQibs\n"
-			//	"OtjTyslfCi8JcjaQMFiH9dH4pJ40CH6VZdNg07KLTiEmbaylBRloXOYa1nSNM2BH\n"
-			//	"xPb/L/T3rQTFCCDn9D03LUYiIMkc4OfgD3zGYpqmn8cfFqZzeQrNZaSvY+64J0EP\n"
-			//	"d0fnZlfLUQKBgCqC9BPUHQpENIKhH4h861dQ9uli+u+/nKFB2aE7Z+Q6k8/cHGyp\n"
-			//	"xlFPI3sCFy2dQ9msaQFaMZvYxX8etpXDZB214a/hCL+J03upJNTZFWuD9AAvnro6\n"
-			//	"IubZEKN/aI8tJDU0Il419s696bC3cqc0fIqmrIJuFF9RN6oBlWDK8KUXAoGBAKCy\n"
-			//	"/U1OLoLPZga9pZDD4pltUBouZEHa+XH9460MYu3ePj/S3o2HuTPkZbuF2pttAVmS\n"
-			//	"imaFj7HyQn7aTzNurIRhllV9NnDWt/I88KFrOQW1TISJ9kcpSIK8hhsLTWA/wxDx\n"
-			//	"9aTH4xXzGg4tizwHh0cponAlvQ28f60V3k1dzhTxAoGBALPKBs+piYWAzhqC4WPn\n"
-			//	"XH7HUpFoG+ucTmx+OCJVZGDTf5ytaEo1FIh3wjNAMRFh8zZUCtPvB3qVv0+1SmTd\n"
-			//	"/zKOY1Mq/eorDx+yRv+03jePpQdGkLU68ivS/o09pGW5d7fsc/o4WAwVMBhGjhdz\n"
-			//	"yDaivtYrC50cRkbfID1GGfwO\n"
-			//	"-----END PRIVATE KEY-----\n";
-
-			//ctx.use_private_key(
-			//	boost::asio::buffer(key.data(), key.size()),
-			//	boost::asio::ssl::context::file_format::pem);
-
-			//std::string const dh =
-			//	"-----BEGIN DH PARAMETERS-----\n"
-			//	"MIIBCAKCAQEAlBXVrjnzFxgRwPAweVOCIFHMbeuQ/DPhrsNB+5GYpcPKfELHwdbE\n"
-			//	"rbxbhAKEO+1jeN0g+P6fH4KSjMvUqkA4LeijJ1b5BJ/Zr2eZf8NKgCpZ6Hm81/sQ\n"
-			//	"5nGvT1ZNRh0h1Q1vUIYfH1OFzonCfD7SQNPxZhf71jK3ii9gN7JJUjtAjOmNyqzI\n"
-			//	"LPYF4IY/t2TfAZGHMMOqQ3rhTudv2VNMwoIB0HpUTvhDQixkY4aaFj1ge5DsJukF\n"
-			//	"qzEWFdZsHK5/hklMyKrWHsipkZZYsNT5OcD1Qp4BskhluwKVrcobk0tIGQg2jIO1\n"
-			//	"VC6cQt++ZUl83Ox/c04wVICnPdVgIznPYwIBAg==\n"
-			//	"-----END DH PARAMETERS-----\n";
-
-			//ctx.use_tmp_dh(
-			//	boost::asio::buffer(dh.data(), dh.size()));
-
-			std::string const cert =
-				"-----BEGIN CERTIFICATE-----\n"
-				"MIIDaDCCAlCgAwIBAgIJAO8vBu8i8exWMA0GCSqGSIb3DQEBCwUAMEkxCzAJBgNV\n"
-				"BAYTAlVTMQswCQYDVQQIDAJDQTEtMCsGA1UEBwwkTG9zIEFuZ2VsZXNPPUJlYXN0\n"
-				"Q049d3d3LmV4YW1wbGUuY29tMB4XDTE3MDUwMzE4MzkxMloXDTQ0MDkxODE4Mzkx\n"
-				"MlowSTELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMS0wKwYDVQQHDCRMb3MgQW5n\n"
-				"ZWxlc089QmVhc3RDTj13d3cuZXhhbXBsZS5jb20wggEiMA0GCSqGSIb3DQEBAQUA\n"
-				"A4IBDwAwggEKAoIBAQDJ7BRKFO8fqmsEXw8v9YOVXyrQVsVbjSSGEs4Vzs4cJgcF\n"
-				"xqGitbnLIrOgiJpRAPLy5MNcAXE1strVGfdEf7xMYSZ/4wOrxUyVw/Ltgsft8m7b\n"
-				"Fu8TsCzO6XrxpnVtWk506YZ7ToTa5UjHfBi2+pWTxbpN12UhiZNUcrRsqTFW+6fO\n"
-				"9d7xm5wlaZG8cMdg0cO1bhkz45JSl3wWKIES7t3EfKePZbNlQ5hPy7Pd5JTmdGBp\n"
-				"yY8anC8u4LPbmgW0/U31PH0rRVfGcBbZsAoQw5Tc5dnb6N2GEIbq3ehSfdDHGnrv\n"
-				"enu2tOK9Qx6GEzXh3sekZkxcgh+NlIxCNxu//Dk9AgMBAAGjUzBRMB0GA1UdDgQW\n"
-				"BBTZh0N9Ne1OD7GBGJYz4PNESHuXezAfBgNVHSMEGDAWgBTZh0N9Ne1OD7GBGJYz\n"
-				"4PNESHuXezAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQCmTJVT\n"
-				"LH5Cru1vXtzb3N9dyolcVH82xFVwPewArchgq+CEkajOU9bnzCqvhM4CryBb4cUs\n"
-				"gqXWp85hAh55uBOqXb2yyESEleMCJEiVTwm/m26FdONvEGptsiCmF5Gxi0YRtn8N\n"
-				"V+KhrQaAyLrLdPYI7TrwAOisq2I1cD0mt+xgwuv/654Rl3IhOMx+fKWKJ9qLAiaE\n"
-				"fQyshjlPP9mYVxWOxqctUdQ8UnsUKKGEUcVrA08i1OAnVKlPFjKBvk+r7jpsTPcr\n"
-				"9pWXTO9JrYMML7d+XRSZA1n3856OqZDX4403+9FnXCvfcLZLLKTBvwwFgEFGpzjK\n"
-				"UEVbkhd5qstF6qWK\n"
-				"-----END CERTIFICATE-----\n";
-
-			std::string const key =
-				"-----BEGIN PRIVATE KEY-----\n"
-				"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDJ7BRKFO8fqmsE\n"
-				"Xw8v9YOVXyrQVsVbjSSGEs4Vzs4cJgcFxqGitbnLIrOgiJpRAPLy5MNcAXE1strV\n"
-				"GfdEf7xMYSZ/4wOrxUyVw/Ltgsft8m7bFu8TsCzO6XrxpnVtWk506YZ7ToTa5UjH\n"
-				"fBi2+pWTxbpN12UhiZNUcrRsqTFW+6fO9d7xm5wlaZG8cMdg0cO1bhkz45JSl3wW\n"
-				"KIES7t3EfKePZbNlQ5hPy7Pd5JTmdGBpyY8anC8u4LPbmgW0/U31PH0rRVfGcBbZ\n"
-				"sAoQw5Tc5dnb6N2GEIbq3ehSfdDHGnrvenu2tOK9Qx6GEzXh3sekZkxcgh+NlIxC\n"
-				"Nxu//Dk9AgMBAAECggEBAK1gV8uETg4SdfE67f9v/5uyK0DYQH1ro4C7hNiUycTB\n"
-				"oiYDd6YOA4m4MiQVJuuGtRR5+IR3eI1zFRMFSJs4UqYChNwqQGys7CVsKpplQOW+\n"
-				"1BCqkH2HN/Ix5662Dv3mHJemLCKUON77IJKoq0/xuZ04mc9csykox6grFWB3pjXY\n"
-				"OEn9U8pt5KNldWfpfAZ7xu9WfyvthGXlhfwKEetOuHfAQv7FF6s25UIEU6Hmnwp9\n"
-				"VmYp2twfMGdztz/gfFjKOGxf92RG+FMSkyAPq/vhyB7oQWxa+vdBn6BSdsfn27Qs\n"
-				"bTvXrGe4FYcbuw4WkAKTljZX7TUegkXiwFoSps0jegECgYEA7o5AcRTZVUmmSs8W\n"
-				"PUHn89UEuDAMFVk7grG1bg8exLQSpugCykcqXt1WNrqB7x6nB+dbVANWNhSmhgCg\n"
-				"VrV941vbx8ketqZ9YInSbGPWIU/tss3r8Yx2Ct3mQpvpGC6iGHzEc/NHJP8Efvh/\n"
-				"CcUWmLjLGJYYeP5oNu5cncC3fXUCgYEA2LANATm0A6sFVGe3sSLO9un1brA4zlZE\n"
-				"Hjd3KOZnMPt73B426qUOcw5B2wIS8GJsUES0P94pKg83oyzmoUV9vJpJLjHA4qmL\n"
-				"CDAd6CjAmE5ea4dFdZwDDS8F9FntJMdPQJA9vq+JaeS+k7ds3+7oiNe+RUIHR1Sz\n"
-				"VEAKh3Xw66kCgYB7KO/2Mchesu5qku2tZJhHF4QfP5cNcos511uO3bmJ3ln+16uR\n"
-				"GRqz7Vu0V6f7dvzPJM/O2QYqV5D9f9dHzN2YgvU9+QSlUeFK9PyxPv3vJt/WP1//\n"
-				"zf+nbpaRbwLxnCnNsKSQJFpnrE166/pSZfFbmZQpNlyeIuJU8czZGQTifQKBgHXe\n"
-				"/pQGEZhVNab+bHwdFTxXdDzr+1qyrodJYLaM7uFES9InVXQ6qSuJO+WosSi2QXlA\n"
-				"hlSfwwCwGnHXAPYFWSp5Owm34tbpp0mi8wHQ+UNgjhgsE2qwnTBUvgZ3zHpPORtD\n"
-				"23KZBkTmO40bIEyIJ1IZGdWO32q79nkEBTY+v/lRAoGBAI1rbouFYPBrTYQ9kcjt\n"
-				"1yfu4JF5MvO9JrHQ9tOwkqDmNCWx9xWXbgydsn/eFtuUMULWsG3lNjfst/Esb8ch\n"
-				"k5cZd6pdJZa4/vhEwrYYSuEjMCnRb0lUsm7TsHxQrUd6Fi/mUuFU/haC0o0chLq7\n"
-				"pVOUFq5mW8p0zbtfHbjkgxyF\n"
-				"-----END PRIVATE KEY-----\n";
-
-			std::string const dh =
-				"-----BEGIN DH PARAMETERS-----\n"
-				"MIIBCAKCAQEArzQc5mpm0Fs8yahDeySj31JZlwEphUdZ9StM2D8+Fo7TMduGtSi+\n"
-				"/HRWVwHcTFAgrxVdm+dl474mOUqqaz4MpzIb6+6OVfWHbQJmXPepZKyu4LgUPvY/\n"
-				"4q3/iDMjIS0fLOu/bLuObwU5ccZmDgfhmz1GanRlTQOiYRty3FiOATWZBRh6uv4u\n"
-				"tff4A9Bm3V9tLx9S6djq31w31Gl7OQhryodW28kc16t9TvO1BzcV3HjRPwpe701X\n"
-				"oEEZdnZWANkkpR/m/pfgdmGPU66S2sXMHgsliViQWpDCYeehrvFRHEdR9NV+XJfC\n"
-				"QMUk26jPTIVTLfXmmwU0u8vUkpR7LQKkwwIBAg==\n"
-				"-----END DH PARAMETERS-----\n";
-			ctx.use_certificate_chain(
-				boost::asio::buffer(cert.data(), cert.size()));
-
-			ctx.use_private_key(
-				boost::asio::buffer(key.data(), key.size()),
-				boost::asio::ssl::context::file_format::pem);
-
-			ctx.use_tmp_dh(
-				boost::asio::buffer(dh.data(), dh.size()));
+// 			std::string const cert =
+// 				"-----BEGIN CERTIFICATE-----\n"
+// 				"MIIDaDCCAlCgAwIBAgIJAO8vBu8i8exWMA0GCSqGSIb3DQEBCwUAMEkxCzAJBgNV\n"
+// 				"BAYTAlVTMQswCQYDVQQIDAJDQTEtMCsGA1UEBwwkTG9zIEFuZ2VsZXNPPUJlYXN0\n"
+// 				"Q049d3d3LmV4YW1wbGUuY29tMB4XDTE3MDUwMzE4MzkxMloXDTQ0MDkxODE4Mzkx\n"
+// 				"MlowSTELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMS0wKwYDVQQHDCRMb3MgQW5n\n"
+// 				"ZWxlc089QmVhc3RDTj13d3cuZXhhbXBsZS5jb20wggEiMA0GCSqGSIb3DQEBAQUA\n"
+// 				"A4IBDwAwggEKAoIBAQDJ7BRKFO8fqmsEXw8v9YOVXyrQVsVbjSSGEs4Vzs4cJgcF\n"
+// 				"xqGitbnLIrOgiJpRAPLy5MNcAXE1strVGfdEf7xMYSZ/4wOrxUyVw/Ltgsft8m7b\n"
+// 				"Fu8TsCzO6XrxpnVtWk506YZ7ToTa5UjHfBi2+pWTxbpN12UhiZNUcrRsqTFW+6fO\n"
+// 				"9d7xm5wlaZG8cMdg0cO1bhkz45JSl3wWKIES7t3EfKePZbNlQ5hPy7Pd5JTmdGBp\n"
+// 				"yY8anC8u4LPbmgW0/U31PH0rRVfGcBbZsAoQw5Tc5dnb6N2GEIbq3ehSfdDHGnrv\n"
+// 				"enu2tOK9Qx6GEzXh3sekZkxcgh+NlIxCNxu//Dk9AgMBAAGjUzBRMB0GA1UdDgQW\n"
+// 				"BBTZh0N9Ne1OD7GBGJYz4PNESHuXezAfBgNVHSMEGDAWgBTZh0N9Ne1OD7GBGJYz\n"
+// 				"4PNESHuXezAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQCmTJVT\n"
+// 				"LH5Cru1vXtzb3N9dyolcVH82xFVwPewArchgq+CEkajOU9bnzCqvhM4CryBb4cUs\n"
+// 				"gqXWp85hAh55uBOqXb2yyESEleMCJEiVTwm/m26FdONvEGptsiCmF5Gxi0YRtn8N\n"
+// 				"V+KhrQaAyLrLdPYI7TrwAOisq2I1cD0mt+xgwuv/654Rl3IhOMx+fKWKJ9qLAiaE\n"
+// 				"fQyshjlPP9mYVxWOxqctUdQ8UnsUKKGEUcVrA08i1OAnVKlPFjKBvk+r7jpsTPcr\n"
+// 				"9pWXTO9JrYMML7d+XRSZA1n3856OqZDX4403+9FnXCvfcLZLLKTBvwwFgEFGpzjK\n"
+// 				"UEVbkhd5qstF6qWK\n"
+// 				"-----END CERTIFICATE-----\n";
+// 
+// 			std::string const key =
+// 				"-----BEGIN PRIVATE KEY-----\n"
+// 				"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDJ7BRKFO8fqmsE\n"
+// 				"Xw8v9YOVXyrQVsVbjSSGEs4Vzs4cJgcFxqGitbnLIrOgiJpRAPLy5MNcAXE1strV\n"
+// 				"GfdEf7xMYSZ/4wOrxUyVw/Ltgsft8m7bFu8TsCzO6XrxpnVtWk506YZ7ToTa5UjH\n"
+// 				"fBi2+pWTxbpN12UhiZNUcrRsqTFW+6fO9d7xm5wlaZG8cMdg0cO1bhkz45JSl3wW\n"
+// 				"KIES7t3EfKePZbNlQ5hPy7Pd5JTmdGBpyY8anC8u4LPbmgW0/U31PH0rRVfGcBbZ\n"
+// 				"sAoQw5Tc5dnb6N2GEIbq3ehSfdDHGnrvenu2tOK9Qx6GEzXh3sekZkxcgh+NlIxC\n"
+// 				"Nxu//Dk9AgMBAAECggEBAK1gV8uETg4SdfE67f9v/5uyK0DYQH1ro4C7hNiUycTB\n"
+// 				"oiYDd6YOA4m4MiQVJuuGtRR5+IR3eI1zFRMFSJs4UqYChNwqQGys7CVsKpplQOW+\n"
+// 				"1BCqkH2HN/Ix5662Dv3mHJemLCKUON77IJKoq0/xuZ04mc9csykox6grFWB3pjXY\n"
+// 				"OEn9U8pt5KNldWfpfAZ7xu9WfyvthGXlhfwKEetOuHfAQv7FF6s25UIEU6Hmnwp9\n"
+// 				"VmYp2twfMGdztz/gfFjKOGxf92RG+FMSkyAPq/vhyB7oQWxa+vdBn6BSdsfn27Qs\n"
+// 				"bTvXrGe4FYcbuw4WkAKTljZX7TUegkXiwFoSps0jegECgYEA7o5AcRTZVUmmSs8W\n"
+// 				"PUHn89UEuDAMFVk7grG1bg8exLQSpugCykcqXt1WNrqB7x6nB+dbVANWNhSmhgCg\n"
+// 				"VrV941vbx8ketqZ9YInSbGPWIU/tss3r8Yx2Ct3mQpvpGC6iGHzEc/NHJP8Efvh/\n"
+// 				"CcUWmLjLGJYYeP5oNu5cncC3fXUCgYEA2LANATm0A6sFVGe3sSLO9un1brA4zlZE\n"
+// 				"Hjd3KOZnMPt73B426qUOcw5B2wIS8GJsUES0P94pKg83oyzmoUV9vJpJLjHA4qmL\n"
+// 				"CDAd6CjAmE5ea4dFdZwDDS8F9FntJMdPQJA9vq+JaeS+k7ds3+7oiNe+RUIHR1Sz\n"
+// 				"VEAKh3Xw66kCgYB7KO/2Mchesu5qku2tZJhHF4QfP5cNcos511uO3bmJ3ln+16uR\n"
+// 				"GRqz7Vu0V6f7dvzPJM/O2QYqV5D9f9dHzN2YgvU9+QSlUeFK9PyxPv3vJt/WP1//\n"
+// 				"zf+nbpaRbwLxnCnNsKSQJFpnrE166/pSZfFbmZQpNlyeIuJU8czZGQTifQKBgHXe\n"
+// 				"/pQGEZhVNab+bHwdFTxXdDzr+1qyrodJYLaM7uFES9InVXQ6qSuJO+WosSi2QXlA\n"
+// 				"hlSfwwCwGnHXAPYFWSp5Owm34tbpp0mi8wHQ+UNgjhgsE2qwnTBUvgZ3zHpPORtD\n"
+// 				"23KZBkTmO40bIEyIJ1IZGdWO32q79nkEBTY+v/lRAoGBAI1rbouFYPBrTYQ9kcjt\n"
+// 				"1yfu4JF5MvO9JrHQ9tOwkqDmNCWx9xWXbgydsn/eFtuUMULWsG3lNjfst/Esb8ch\n"
+// 				"k5cZd6pdJZa4/vhEwrYYSuEjMCnRb0lUsm7TsHxQrUd6Fi/mUuFU/haC0o0chLq7\n"
+// 				"pVOUFq5mW8p0zbtfHbjkgxyF\n"
+// 				"-----END PRIVATE KEY-----\n";
+// 
+// 			std::string const dh =
+// 				"-----BEGIN DH PARAMETERS-----\n"
+// 				"MIIBCAKCAQEArzQc5mpm0Fs8yahDeySj31JZlwEphUdZ9StM2D8+Fo7TMduGtSi+\n"
+// 				"/HRWVwHcTFAgrxVdm+dl474mOUqqaz4MpzIb6+6OVfWHbQJmXPepZKyu4LgUPvY/\n"
+// 				"4q3/iDMjIS0fLOu/bLuObwU5ccZmDgfhmz1GanRlTQOiYRty3FiOATWZBRh6uv4u\n"
+// 				"tff4A9Bm3V9tLx9S6djq31w31Gl7OQhryodW28kc16t9TvO1BzcV3HjRPwpe701X\n"
+// 				"oEEZdnZWANkkpR/m/pfgdmGPU66S2sXMHgsliViQWpDCYeehrvFRHEdR9NV+XJfC\n"
+// 				"QMUk26jPTIVTLfXmmwU0u8vUkpR7LQKkwwIBAg==\n"
+// 				"-----END DH PARAMETERS-----\n";
+// 			ctx.use_certificate_chain(
+// 				boost::asio::buffer(cert.data(), cert.size()));
+// 
+// 			ctx.use_private_key(
+// 				boost::asio::buffer(key.data(), key.size()),
+// 				boost::asio::ssl::context::file_format::pem);
+// 
+// 			ctx.use_tmp_dh(
+// 				boost::asio::buffer(dh.data(), dh.size()));
 
 		}
 		catch (std::exception const& ex) {
