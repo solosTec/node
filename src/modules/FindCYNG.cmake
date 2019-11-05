a3bc4c65fabbf5b15147c758348c9deb09dc893c
sylko.olzscher <CmdPirx@gmail.com>
2019-11-05 20:57:42 +0100

    FindCYNG reworked

--- src/modules/FindCYNG.cmake
+++ src/modules/FindCYNG.cmake
@@ -26,8 +26,11 @@ else(CYNG_PKG)
 
     set(__CYNG_DEVELOP_ROOT "${PROJECT_SOURCE_DIR}/../cyng" CACHE PATH "__CYNG_DEVELOP_ROOT")
     get_filename_component(CYNG_DEVELOP_ROOT ${__CYNG_DEVELOP_ROOT} REALPATH)
-    message(STATUS "** search for developer path: ${CYNG_DEVELOP_ROOT}")
+    message(STATUS "** search for developer path (CYNG_DEVELOP_ROOT): ${CYNG_DEVELOP_ROOT}")
 
+	#
+	#	cyng header files
+	#
     find_path(CYNG_INCLUDE_DIR_1
         NAMES 
             cyng/cyng.h
@@ -43,9 +46,12 @@ else(CYNG_PKG)
             "CYNG headers"
     )
     
-#     message(STATUS "** found 1: ${CYNG_INCLUDE_DIR}")
+#   message(STATUS "** found 1: ${CYNG_INCLUDE_DIR_1}")
 
-    find_path(CYNG_INCLUDE_DIR_2
+	#
+	#	cyng generated header files
+	#
+   find_path(CYNG_INCLUDE_DIR_2
         NAMES 
             CYNG_project_info.h
          HINTS
@@ -56,65 +62,107 @@ else(CYNG_PKG)
             "CYNG headers"
     )
 
-#     message(STATUS "** found 2: ${CYNG_INCLUDE_DIR_2}")
+	#
+	#	cyng submodule header files
+	#
+   find_path(CYNG_INCLUDE_DIR_3
+        NAMES 
+            pugixml.hpp
+         HINTS
+            "${CYNG_DEVELOP_ROOT}/lib/xml/pugixml/src"
+            /usr/include/
+            /usr/local/include/
+        DOC 
+            "pugixml headers"
+    )
 
-    if (UNIX)
-        set(__CYNG_BUILD "${CYNG_DEVELOP_ROOT}/build" CACHE PATH "__CYNG_BUILD")
-    else()
-        #
-        #	$(ConfigurationName) is a variable used by the MS build system
-        #
-        set(__CYNG_BUILD "${CYNG_DEVELOP_ROOT}/build/$(ConfigurationName)" CACHE PATH "__CYNG_BUILD")
-    endif(UNIX)
-    
-    get_filename_component(CYNG_BUILD ${__CYNG_BUILD} REALPATH)
-#     message(STATUS "** search for build path: ${CYNG_BUILD}")
-
+#    message(STATUS "** found 2: ${CYNG_INCLUDE_DIR_2}")
     # libcyng_async.so   libcyng_csv.so        libcyng_domain.so  libcyng_log.so   libcyng_parser.so  libcyng_store.so  libcyng_vm.so
     # libcyng_core.so    libcyng_db.so         libcyng_io.so      libcyng_mail.so  libcyng_rnd.so     libcyng_sys.so    libcyng_xml.so
     # libcyng_json.so    libcyng_meta.so       libcyng_sql.so     libcyng_table.so
 
-    set(CYNG_LIBRARIES "")
+    #set(CYNG_LIBRARIES "")
     set(CYNG_LIB_LIST "cyng_async;cyng_csv;cyng_domain;cyng_log;cyng_parser;cyng_store;cyng_vm;cyng_core;cyng_db;cyng_io;cyng_crypto;cyng_mail;cyng_rnd;cyng_sys;cyng_xml;cyng_json;cyng_meta;cyng_sql;cyng_table;cyng_sqlite3")
-    
-    foreach(CYNG_LIB ${CYNG_LIB_LIST})
-        find_library("__${CYNG_LIB}" ${CYNG_LIB}
-            HINTS
-                ${CYNG_BUILD}
-            PATHS
-                /usr/lib/
-                /usr/local/lib
-            DOC 
-                "CYNG libraries"
-        )
-        
-        list(APPEND CYNG_LIBRARIES ${__${CYNG_LIB}})
-#         message(STATUS "** __CYNG_LIB    : ${__${CYNG_LIB}}")
-    endforeach()
 
+    if (UNIX)
+        set(__CYNG_BUILD "${CYNG_DEVELOP_ROOT}/build" CACHE PATH "__CYNG_BUILD")
+
+		foreach(CYNG_LIB ${CYNG_LIB_LIST})
+			find_library("__${CYNG_LIB}" ${CYNG_LIB}
+				HINTS
+					${CYNG_BUILD}
+				PATHS
+					/usr/lib/
+					/usr/local/lib
+				DOC 
+					"CYNG libraries"
+			)
+        
+			list(APPEND CYNG_LIBRARIES ${__${CYNG_LIB}})
+			#message(STATUS "** __CYNG_LIB    : ${__${CYNG_LIB}}")
+		endforeach()
+
+    else()
+        #
+        #	$(ConfigurationName) is a variable used by the MS build system
+        #
+        set(__CYNG_BUILD_OPTIMIZED "${CYNG_DEVELOP_ROOT}/build/Release" CACHE PATH "__CYNG_BUILD_OPTIMIZED")
+        set(__CYNG_BUILD_DEBUG "${CYNG_DEVELOP_ROOT}/build/Debug" CACHE PATH "__CYNG_BUILD_DEBUG")
+
+		foreach(CYNG_LIB ${CYNG_LIB_LIST})
+
+		#
+		#	optimized library
+		#
+			list(APPEND CYNG_LIBRARIES "optimized")
+			list(APPEND CYNG_LIBRARIES "${__CYNG_BUILD_OPTIMIZED}/${CYNG_LIB}.lib")
+
+		#
+		#	debug library
+		#
+			list(APPEND CYNG_LIBRARIES "debug")
+			list(APPEND CYNG_LIBRARIES "${__CYNG_BUILD_DEBUG}/${CYNG_LIB}.lib")
+
+			#message(STATUS "** CYNG_LIBRARIES    : ${CYNG_LIBRARIES}")
+
+		endforeach()
+
+    endif(UNIX)
+    
+    
     #
     #  remove the cyng prefix - maybe there is a better solution
     #
-    get_filename_component(CYNG_INCLUDE_DIR_1 "${CYNG_INCLUDE_DIR_1}/../" REALPATH)
-    set(CYNG_INCLUDE_DIRS "${CYNG_INCLUDE_DIR_1};${CYNG_INCLUDE_DIR_2}")
+    if (UNIX)
+		get_filename_component(CYNG_INCLUDE_DIR_1 "${CYNG_INCLUDE_DIR_1}/../" REALPATH)
+	endif()
+	set(CYNG_INCLUDE_DIRS "${CYNG_INCLUDE_DIR_1};${CYNG_INCLUDE_DIR_2};${CYNG_INCLUDE_DIR_3}")
     
     get_filename_component(CYNG_MODULE_PATH "${CYNG_DEVELOP_ROOT}/src/modules/" REALPATH)
     set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CYNG_MODULE_PATH}")
 #     message(STATUS "** CMake modules     : ${CMAKE_MODULE_PATH}")
+
+	message(STATUS "** CYNG_INCLUDE_DIRS     : ${CYNG_INCLUDE_DIRS}")
+	message(STATUS "** CYNG_LIBRARIES     : ${CYNG_LIBRARIES}")
+	if (CYNG_INCLUDE_DIRS AND CYNG_LIBRARIES)
+		set(CYNG_FOUND ON)
+	endif()
     
 endif(CYNG_PKG_FOUND)
 
 
 mark_as_advanced(CYNG_FOUND CYNG_INCLUDE_DIRS CYNG_LIBRARIES)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CYNG
-    REQUIRED_VARS 
-        CYNG_INCLUDE_DIRS
-        CYNG_LIBRARIES
-    VERSION_VAR 
-        CYNG_VERSION
-)
+if(UNIX)
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(CYNG
+		REQUIRED_VARS 
+			CYNG_INCLUDE_DIRS
+			CYNG_LIBRARIES
+		VERSION_VAR 
+			CYNG_VERSION
+	)
+endif(UNIX)
 
 # if(CYNG_FOUND AND NOT TARGET CYNG::CYNG)
 #     add_library(CYNG::CYNG INTERFACE IMPORTED)
