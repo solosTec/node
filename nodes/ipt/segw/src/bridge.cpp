4657c8520b14a5c4ff27274d84221ceacaa7e942
Sylko Olzscher <sylko.olzscher@gmail.com>
2020-01-20 18:13:15 +0100

    don't use GPIOs when disabled

--- nodes/ipt/segw/src/bridge.cpp
+++ nodes/ipt/segw/src/bridge.cpp
@@ -527,22 +527,28 @@ namespace node
 		//gpio-path|1|/sys/class/gpio|/sys/class/gpio|15
 		//gpio-vector|1|46 47 50 53|46 47 50 53|15
 
+		auto const gpio_enabled = cache_.get_cfg("gpio-enabled", false);
 		auto const gpio_path = cache_.get_cfg<std::string>("gpio-path", "/sys/class/gpio");
 		auto const gpio_vector = cache_.get_cfg<std::string>("gpio-vector", "46 47 50 53");
 
-		//
-		//	Start a GPIO task for every GPIO
-		//
-		auto const svec = cyng::split(gpio_vector, " ");
-		for (auto const& s : svec) {
-			auto const tid = cyng::async::start_task_detached<gpio>(mux
-				, logger_
-				, boost::filesystem::path(gpio_path) / ("/gpio" + s));
+		if (gpio_enabled) {
+			//
+			//	Start a GPIO task for every GPIO
+			//
+			auto const svec = cyng::split(gpio_vector, " ");
+			for (auto const& s : svec) {
+				auto const tid = cyng::async::start_task_detached<gpio>(mux
+					, logger_
+					, boost::filesystem::path(gpio_path) / ("/gpio" + s));
 
-			//
-			//	store task id in cache DB
-			//
-			cache_.set_cfg("gpio-task-" + s, tid);
+				//
+				//	store task id in cache DB
+				//
+				cache_.set_cfg("gpio-task-" + s, tid);
+			}
+		}
+		else {
+			CYNG_LOG_WARNING(logger_, "GPIO control disabled");
 		}
 	}
 
