fe25c868f6557e0f0fc8963ddd6f154f6f32a876
Paul Jaehne <p.jaehne@ppc-ag.de>
2022-08-21 15:54:56 +0200

    integrate build into ptxdist

--- cmake/FindCYNG.cmake
+++ cmake/FindCYNG.cmake
@@ -5,29 +5,27 @@
 # This will define the following variables
 #
 #   CYNG_FOUND          - system has cyng
-#   CYNG_INCLUDE_DIRS   - the cyng include directories
+#   CYNG_INCLUDE_DIRS	- the cyng include directories
 #   CYNG_LIBRARIES      - cyng libraries directories
+#	<NAME>_LIBRARY			- individual variable for each library found
 #
 # and the following imported targets
 #
-#   cyng::cyng
-#	cyng::db
-#	cyng::io
-#	cyng::log
-#	cyng::obj
-#	cyng::parse
-#	cyng::rnd
-#	cyng::sql
-#	cyng::store
-#	cyng::sys
-#	cyng::task
-#	cyng::vm
-#	cyng::net
-#	cyng::sqlite3
+#     CYNG::CYNG
 #
 
-# mostly [build]
-message(STATUS "** CYNG build tree          : [${SMF_BUILD_TREE_STEM}]")
+# clear the variables, in case they were set somewhere elese
+if(CYNG_LIBRARIES)
+	unset(CYNG_LIBRARIES)
+	unset(CYNG_LIBRARIES CACHE)
+	unset(CYNG_LIBRARIES PARENT_SCOPE)
+endif()
+
+if(CYNG_INCLUDE_DIRS)
+	unset(CYNG_INCLUDE_DIRS)
+	unset(CYNG_INCLUDE_DIRS CACHE)
+	unset(CYNG_INCLUDE_DIRS PARENT_SCOPE)
+endif()
 
 #
 #	try PkgConfig
@@ -36,48 +34,39 @@ find_package(PkgConfig)
 pkg_check_modules(PC_CYNG QUIET CYNG)
 
 if(PC_CYNG_FOUND)
-    # the library was already found by pkgconfig
     set(CYNG_VERSION ${PC_CYNG_VERSION})
     set(CYNG_INCLUDE_DIRS ${PC_CYNG_INCLUDE_DIRS})
     set(CYNG_LIBRARIES ${PC_CYNG_LIBRARIES})
-     
-    if(NOT PC_CYNG_QUIETLY)
-        message(STATUS "** CYNG_PKG_FOUND: BEGIN")
-        message(STATUS "** CYNG_VERSION: ${CYNG_VERSION}")
-        message(STATUS "** CYNG_INCLUDE_DIRS: ${CYNG_INCLUDE_DIRS}")
-        message(STATUS "** CYNG_LIBRARIES: ${CYNG_LIBRARIES}")
-        message(STATUS "** CYNG_PKG_FOUND: END")
-    endif(NOT PC_CYNG_QUIETLY)
+
+	if(NOT PC_CYNGQUIETLY)
+		message(STATUS "** PC_CYNG_FOUND: BEGIN")
+		message(STATUS "** CYNG_VERSION: ${CYNG_VERSION}")
+		message(STATUS "** CYNG_INCLUDE_DIRS: ${CYNG_INCLUDE_DIRS}")
+		message(STATUS "** CYNG_LIBRARIES: ${CYNG_LIBRARIES}")
+		message(STATUS "** PC_CYNG_FOUND: END")
+	endif(NOT PC_CYNG_QUIETLY)
 
 else(PC_CYNG_FOUND)
 
 	#
+	#	cyng header files
+	#
+	file(GLOB CYNG_SEARCH_PATH 
+		"${CMAKE_PREFIX_PATH}/cyng*" 
+		"${PROJECT_SOURCE_DIR}/../cyng*" 
+		"${PROJECT_SOURCE_DIR}/../../sysroot-target" 
+        "${PROJECT_SOURCE_DIR}/../../packages/cyng*"
+		"${PROJECT_SOURCE_DIR}/../../root")
+
+	#
 	#	cyng header files, which are included in the form cyng/xx.h
 	#
-	file(GLOB CYNG_SEARCH_PATH "${CMAKE_PREFIX_PATH}/cyng*" "${PROJECT_SOURCE_DIR}/../cyng*" "${PROJECT_SOURCE_DIR}/../../sysroot-target")
     find_path(CYNG_INCLUDE_DIR_SRC
         NAMES 
-			cyng/meta.hpp
+			cyng/meta.hpp 
             cyng/obj/object.h
-        PATH_SUFFIXES
-            include
-        PATHS
-            ${CYNG_SEARCH_PATH}
-        DOC 
-            "CYNG headers"
-		NO_CMAKE_FIND_ROOT_PATH
-    )
-    
-	#
-	#	cyng generated header files
-	#
-    find_path(CYNG_INCLUDE_DIR_BUILD
-        NAMES 
-            cyng.h
-        PATH_SUFFIXES
+		PATH_SUFFIXES
 			include
-			${SMF_BUILD_TREE_STEM}/include
-			build/include
         PATHS
 			${CYNG_SEARCH_PATH}
         DOC 
@@ -85,108 +74,78 @@ else(PC_CYNG_FOUND)
 		NO_CMAKE_FIND_ROOT_PATH
     )
 
-	if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.20") 
-		cmake_path(GET CYNG_INCLUDE_DIR_BUILD PARENT_PATH CYNG_BUILD_DIR)
-	else()
-		get_filename_component(CYNG_BUILD_DIR ${CYNG_INCLUDE_DIR_BUILD} DIRECTORY)
-	endif()
-	message(STATUS "** CYNG_BUILD_DIR           : ${CYNG_BUILD_DIR}")
-	message(STATUS "** CYNG_SEARCH_PATH         : ${CYNG_SEARCH_PATH}")
-
-    find_library(CYNG_PUGI_XML_LIB
+	#
+	#	cyng generated header files
+	#
+    find_path(CYNG_INCLUDE_DIR_BUILD
         NAMES 
-			pugixml
-        PATHS
-			"/usr"
-			"/usr/local"
-			${CYNG_BUILD_DIR}
-            ${CYNG_SEARCH_PATH}
-			"/src/cyng/build"
+            cyng.h
         PATH_SUFFIXES
-			"lib"
-			"lib64"
-			"lib/x86_64-linux-gnu"
- 			"_deps/pugixml-build"
-			"_deps/pugixml-build/Debug"
-			"_deps/pugixml-build/Release"
+			"include"
+			"${SMFSEC_BUILD_TREE_STEM}/include"
+			"build/include"
+        PATHS
+			${CYNG_SEARCH_PATH}
         DOC 
-            "PugiXML"
-    )
-	message(STATUS "** CYNG_PUGI_XML_LIB        : ${CYNG_PUGI_XML_LIB}")
+            "CYNG headers"
+		NO_CMAKE_FIND_ROOT_PATH
+    )	
 
+
+    list(APPEND CYNG_INCLUDE_DIRS ${CYNG_INCLUDE_DIR_SRC})
+    list(APPEND CYNG_INCLUDE_DIRS ${CYNG_INCLUDE_DIR_BUILD})
+
+    #
+	#	search cyng libraries on linux
 	#
-	#	Search cyng libraries.
-    #   In a multiplatform environment the OECP(1) builds are located in the "v5te" directory
-    #   and this directory is preferred.
-	#	On Windows the Debug build is preferred.
-	#
-    set(REQUESTED_LIBS "db;io;log;obj;parse;rnd;sql;store;sys;task;vm;net;xml;sqlite3")
-    
+	set(FIND_LIBS "cyng_db;cyng_io;cyng_log;cyng_net;cyng_obj;cyng_parse;cyng_rnd;cyng_sql;cyng_store;cyng_sys;cyng_task;cyng_vm;cyng_sqlite3;cyng_xml")
+
 	if(WIN32)
-        list(APPEND REQUESTED_LIBS "scm")
+        list(APPEND FIND_LIBS "cyng_scm")
     endif(WIN32)
 
-	# D:/reboot/node/../cyng
-	message(STATUS "** CYNG_SEARCH_PATH         : ${CYNG_SEARCH_PATH}")
-
-	foreach(__LIB ${REQUESTED_LIBS})
-
-#		message(STATUS "** find         : cyng_${__LIB}")
-#		message(STATUS "** here         : ${SMF_BUILD_TREE_STEM}/src/${__LIB}")
-
-		find_library("__CYNG_${__LIB}" 
-			NAME
-				"cyng_${__LIB}"
-            PATHS
-                ${CYNG_SEARCH_PATH}
-            PATH_SUFFIXES
-				"lib"
-                "usr/lib/"
-				"${SMF_BUILD_TREE_STEM}"
-                "${SMF_BUILD_TREE_STEM}/src/${__LIB}"
-				"build/src/${__LIB}"
-                "${SMF_BUILD_TREE_STEM}/src/${__LIB}/Debug"
-                "${SMF_BUILD_TREE_STEM}/src/${__LIB}/Release"
-				"${SMF_BUILD_TREE_STEM}/Debug"
-				"${SMF_BUILD_TREE_STEM}/Release"
+	foreach(__LIB ${FIND_LIBS})
+		find_library("${__LIB}" ${__LIB}
+			PATHS
+				${CYNG_SEARCH_PATH}
+			PATH_SUFFIXES
+				usr/lib
+				lib
+				build
+				"${SMFSEC_BUILD_TREE_STEM}"
+				"${SMFSEC_BUILD_TREE_STEM}/src/${__LIB}"
+                "${SMFSEC_BUILD_TREE_STEM}/src/${__LIB}/Debug"
+                "${SMFSEC_BUILD_TREE_STEM}/src/${__LIB}/Release"
+				"${SMFSEC_BUILD_TREE_STEM}/Debug"
+				"${SMFSEC_BUILD_TREE_STEM}/Release"
 				"build/Debug"
 				"build/Release"
 			DOC 
 				"CYNG libraries"
+            NO_CMAKE_FIND_ROOT_PATH
 		)
-
-#		message(STATUS "** add library cyng::${__LIB}: ${__CYNG_${__LIB}}")
-		add_library("cyng::${__LIB}" INTERFACE IMPORTED)
-		set_target_properties("cyng::${__LIB}" 
-				PROPERTIES
-					INTERFACE_INCLUDE_DIRECTORIES 
-						"${CYNG_INCLUDE_DIRS}"
-					INTERFACE_LINK_LIBRARIES 
-						"${__CYNG_${__LIB}}"
-			)
-
+		
 		# append the found library to the list of all libraries
-		list(APPEND CYNG_LIBRARIES ${__CYNG_${__LIB}})
+		list(APPEND CYNG_LIBRARIES ${${__LIB}})
 
 		# this creates a variable with the name of the searched library, so that it can be included more easily
-		#unset(__LIB_UPPERCASE_NAME)
-		string(TOUPPER "cyng_${__LIB}" __LIB_UPPERCASE_NAME)
-#		message(STATUS "** lib name  : ${__LIB_UPPERCASE_NAME}: ${__CYNG_${__LIB}}")
-		set(${__LIB_UPPERCASE_NAME} ${__CYNG_${__LIB}})
+		unset(__LIB_UPPERCASE_NAME)
+		string(TOUPPER ${__LIB} __LIB_UPPERCASE_NAME)
+		set(${__LIB_UPPERCASE_NAME}_LIBRARY ${${__LIB}})
 	endforeach()
+
 endif(PC_CYNG_FOUND)
-    
+
 # check if both the header and the libraries have been found
-if (CYNG_INCLUDE_DIR_SRC AND CYNG_INCLUDE_DIR_BUILD AND CYNG_LIBRARIES)
-    set(CYNG_INCLUDE_DIRS "${CYNG_INCLUDE_DIR_SRC};${CYNG_INCLUDE_DIR_BUILD}")
-    set(CYNG_FOUND ON)
-    unset(CYNG_INCLUDE_DIR_SRC CACHE)
+if (CYNG_INCLUDE_DIRS AND CYNG_LIBRARIES)
+	set(CYNG_FOUND ON)
+	unset(CYNG_INCLUDE_DIR_SRC CACHE)
 	unset(CYNG_INCLUDE_DIR_BUILD CACHE)
 	if(NOT CYNG_FIND_QUIETLY)
-		message(STATUS "** CYNG_LIBRARIES           : ${CYNG_LIBRARIES}")
-		message(STATUS "** CYNG_INCLUDE_DIRS        : ${CYNG_INCLUDE_DIRS}")
+		message(STATUS "** CYNG_LIBRARIES    : ${CYNG_LIBRARIES}")
+		message(STATUS "** CYNG_INCLUDE_DIRS : ${CYNG_INCLUDE_DIRS}")
 	endif(NOT CYNG_FIND_QUIETLY)
-endif(CYNG_INCLUDE_DIR_SRC AND CYNG_INCLUDE_DIR_BUILD AND CYNG_LIBRARIES)
+endif(CYNG_INCLUDE_DIRS AND CYNG_LIBRARIES)
 
 mark_as_advanced(CYNG_FOUND CYNG_INCLUDE_DIRS CYNG_LIBRARIES)
 
@@ -216,16 +175,5 @@ if(CYNG_FOUND AND NOT TARGET cyng::cyng)
 				"${CYNG_LIBRARIES}"
     )
 
-endif(CYNG_FOUND AND NOT TARGET cyng::cyng)
+endif()
 
-if (CYNG_PUGI_XML_LIB AND WIN32)
-		message(STATUS "** PugiXML library          : ${CYNG_PUGI_XML_LIB}")
-		add_library(pugixml STATIC IMPORTED)
-		set_target_properties(pugixml 
-			PROPERTIES 
-				IMPORTED_LOCATION 
-					${CYNG_PUGI_XML_LIB}
-		)  
-endif(CYNG_PUGI_XML_LIB AND WIN32)
-
-mark_as_advanced (CYNG_PUGI_XML_LIB)
---
+++ cmake/FindPUGIXML.cmake
@@ -0,0 +1,141 @@
+# FindPUGIXML.cmake
+#
+# Finds the PUGIXML library
+#
+# This will define the following variables
+#
+#   PUGIXML_FOUND          - system has pugixml
+#   PUGIXML_INCLUDE_DIRS	- the pugixml include directories
+#   PUGIXML_LIBRARIES      - pugixml libraries directories
+#	<NAME>_LIBRARY			- individual variable for each library found
+#
+# and the following imported targets
+#
+#     PUGIXML::PUGIXML
+#
+
+# clear the variables, in case they were set somewhere elese
+if(PUGIXML_LIBRARIES)
+	unset(PUGIXML_LIBRARIES)
+	unset(PUGIXML_LIBRARIES CACHE)
+	unset(PUGIXML_LIBRARIES PARENT_SCOPE)
+endif()
+
+if(PUGIXML_INCLUDE_DIRS)
+	unset(PUGIXML_INCLUDE_DIRS)
+	unset(PUGIXML_INCLUDE_DIRS CACHE)
+	unset(PUGIXML_INCLUDE_DIRS PARENT_SCOPE)
+endif()
+
+#
+#	try PkgConfig
+#
+find_package(PkgConfig)
+pkg_check_modules(PC_PUGIXML QUIET PUGIXML)
+
+if(PC_PUGIXML_FOUND)
+    set(PUGIXML_VERSION ${PC_PUGIXML_VERSION})
+    set(PUGIXML_INCLUDE_DIRS ${PC_PUGIXML_INCLUDE_DIRS})
+    set(PUGIXML_LIBRARIES ${PC_PUGIXML_LIBRARIES})
+
+	if(NOT PC_PUGIXMLQUIETLY)
+		message(STATUS "** PC_PUGIXML_FOUND: BEGIN")
+		message(STATUS "** PUGIXML_VERSION: ${PUGIXML_VERSION}")
+		message(STATUS "** PUGIXML_INCLUDE_DIRS: ${PUGIXML_INCLUDE_DIRS}")
+		message(STATUS "** PUGIXML_LIBRARIES: ${PUGIXML_LIBRARIES}")
+		message(STATUS "** PC_PUGIXML_FOUND: END")
+	endif(NOT PC_PUGIXML_QUIETLY)
+
+else(PC_PUGIXML_FOUND)
+
+	#
+	#	pugixml header files
+	#
+	file(GLOB PUGIXML_SEARCH_PATH 
+		"${CMAKE_PREFIX_PATH}/pugixml*" 
+		"${PROJECT_SOURCE_DIR}/../pugixml*" 
+		"${PROJECT_SOURCE_DIR}/../../sysroot-target" 
+        "${PROJECT_SOURCE_DIR}/../../packages/pugixml*"
+		"${PROJECT_SOURCE_DIR}/../../root")
+    find_path(PUGIXML_INCLUDE_DIRS
+        NAMES 
+            pugixml.hpp 
+            pugiconfig.hpp
+		PATH_SUFFIXES
+			usr/include
+			include
+        PATHS
+			${PUGIXML_SEARCH_PATH}
+        DOC 
+            "PUGIXML headers"
+		NO_CMAKE_FIND_ROOT_PATH
+    )
+
+    #
+	#	search pugixml libraries on linux
+	#
+	set(FIND_LIBS "pugixml")
+
+	foreach(__LIB ${FIND_LIBS})
+		find_library("${__LIB}" ${__LIB}
+			PATHS
+				${PUGIXML_SEARCH_PATH}
+			PATH_SUFFIXES
+				usr/lib
+				lib
+				build
+			DOC 
+				"PUGIXML libraries"
+            NO_CMAKE_FIND_ROOT_PATH
+		)
+		
+		# append the found library to the list of all libraries
+		list(APPEND PUGIXML_LIBRARIES ${${__LIB}})
+
+		# this creates a variable with the name of the searched library, so that it can be included more easily
+		unset(__LIB_UPPERCASE_NAME)
+		string(TOUPPER ${__LIB} __LIB_UPPERCASE_NAME)
+		set(${__LIB_UPPERCASE_NAME}_LIBRARY ${${__LIB}})
+	endforeach()
+
+endif(PC_PUGIXML_FOUND)
+
+# check if both the header and the libraries have been found
+if (PUGIXML_INCLUDE_DIRS AND PUGIXML_LIBRARIES)
+	set(PUGIXML_FOUND ON)
+	if(NOT PUGIXML_FIND_QUIETLY)
+		message(STATUS "** PUGIXML_LIBRARIES    : ${PUGIXML_LIBRARIES}")
+		message(STATUS "** PUGIXML_INCLUDE_DIRS : ${PUGIXML_INCLUDE_DIRS}")
+	endif(NOT PUGIXML_FIND_QUIETLY)
+endif(PUGIXML_INCLUDE_DIRS AND PUGIXML_LIBRARIES)
+
+mark_as_advanced(PUGIXML_FOUND PUGIXML_INCLUDE_DIRS PUGIXML_LIBRARIES)
+
+if(UNIX)
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(PUGIXML
+		REQUIRED_VARS 
+			PUGIXML_INCLUDE_DIRS
+			PUGIXML_LIBRARIES
+		VERSION_VAR 
+			PUGIXML_VERSION
+		FAIL_MESSAGE
+			"Cannot provide PUGIXML library"
+	)
+endif(UNIX)
+
+if(PUGIXML_FOUND AND NOT TARGET PUGIXML::PUGIXML)
+
+    add_library(PUGIXML::PUGIXML INTERFACE IMPORTED)
+
+#	define a target
+   	set_target_properties(PUGIXML::PUGIXML 
+		PROPERTIES
+			INTERFACE_INCLUDE_DIRECTORIES 
+				"${PUGIXML_INCLUDE_DIRS}"
+			INTERFACE_LINK_LIBRARIES 
+				"${PUGIXML_LIBRARIES}"
+    )
+
+endif()
+
